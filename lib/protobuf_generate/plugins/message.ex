7f3fe9cc951b10957f346ad8f9a0fb76d48cc8dd
AJ Foster <2789166+aj-foster@users.noreply.github.com>
2026-06-25 14:49:09 +0200

    Simplify logic for generating message comments

--- lib/protobuf_generate/plugins/message.ex
+++ lib/protobuf_generate/plugins/message.ex
@@ -377,67 +377,67 @@ defmodule ProtobufGenerate.Plugins.Message do
   defp from_enum(:TYPE_SINT64), do: :sint64
 
   defp get_comments(ctx, desc) do
-    comments =
+    message_comments =
       Comment.get(ctx)
       |> normalize_indentation()
 
-    comments =
-      if comments != "", do: comments, else: "Automatically generated module for #{desc.name}"
+    message_comments =
+      if message_comments == "" do
+        "Automatically generated module for #{desc.name}"
+      else
+        message_comments
+      end
 
-    if comments != "" do
-      fields =
-        Enum.with_index(desc.field, fn field, index ->
-          ctx = Context.append_comment_path(ctx, "2.#{index}")
-          {field.name, field_comment(ctx, field)}
-        end)
-        |> Enum.sort_by(fn {name, _comment} -> name end)
-        |> Enum.map(fn {_name, comment} -> comment end)
+    fields =
+      Enum.with_index(desc.field, fn field, index ->
+        ctx = Context.append_comment_path(ctx, "2.#{index}")
+        {field.name, field_comment(ctx, field)}
+      end)
+      |> Enum.sort_by(fn {name, _comment} -> name end)
+      |> Enum.map(fn {_name, comment} -> comment end)
 
-      field_rows =
-        Enum.map(fields, fn {row, _additional} -> row end)
-        |> Enum.join("\n")
+    field_rows =
+      Enum.map(fields, fn {row, _additional} -> row end)
+      |> Enum.join("\n")
 
-      additional_notes =
-        Enum.reject(fields, fn {_row, additional} -> is_nil(additional) end)
-        |> Enum.map(fn {_row, additional} -> additional end)
-        |> Enum.join("\n")
+    additional_notes =
+      Enum.reject(fields, fn {_row, additional} -> is_nil(additional) end)
+      |> Enum.map(fn {_row, additional} -> additional end)
+      |> Enum.join("\n")
 
-      moduledoc =
-        cond do
-          field_rows == "" ->
-            comments
+    moduledoc =
+      cond do
+        field_rows == "" ->
+          message_comments
 
-          additional_notes == "" ->
-            """
-            #{comments}
+        additional_notes == "" ->
+          """
+          #{message_comments}
 
-            ## Fields
+          ## Fields
 
-            | # | Name | Type | Notes |
-            |---|------|------|-------|
-            #{field_rows}
-            """
+          | # | Name | Type | Notes |
+          |---|------|------|-------|
+          #{field_rows}
+          """
 
-          :else ->
-            """
-            #{comments}
+        :else ->
+          """
+          #{message_comments}
 
-            ## Fields
+          ## Fields
 
-            | # | Name | Type | Notes |
-            |---|------|------|-------|
-            #{field_rows}
+          | # | Name | Type | Notes |
+          |---|------|------|-------|
+          #{field_rows}
 
-            ### Additional Notes
+          ### Additional Notes
 
-            #{additional_notes}
-            """
-        end
+          #{additional_notes}
+          """
+      end
 
-      indent(moduledoc, 2)
-    else
-      ""
-    end
+    indent(moduledoc, 2)
   end
 
   defp field_comment(ctx, field) do
